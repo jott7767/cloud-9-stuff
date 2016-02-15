921ef9af310f50ca4a3f44d519d5c4dca42a0ec0
Jordan <ottjw@yahoo.com>
2016-02-15 20:18:46 +0000

    Initialize repository

---
+++ jordan.rb
@@ -0,0 +1,4 @@
+puts "I could have code like this." #and the comment after is ignored
+#you can also use a comment to 'disable' or comment out a pice of code:
+# puts "this won't run."
+puts "this will run."
